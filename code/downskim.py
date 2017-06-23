0f9830ca5065be8d62d52a35a84c1d984c378df5
Alex Drlica-Wagner <kadrlica@fnal.gov>
2017-06-23 12:10:29 -0500

    Adding 'downskim' utilities to grab data that already exists on a local disk (i.e., BLISS). This parallels the 'download' functionality from the database.

---
+++ code/downskim.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+"""
+Link the raw catalog files
+"""
+__author__ = "Alex Drlica-Wagner"
+
+import os
+import logging
+import subprocess
+import glob
+
+import yaml
+import pandas as pd
+import numpy as np
+import fitsio
+
+from utils import mkdir
+from const import BANDS, TAGS
+
+#from archive.dexp import ObjectsTable
+
+#bliss_temp = '/data/des50.b/data/BLISS/{dirname}/{expnum}/D00{expnum}_*_fullcat.fits'
+bliss_des50 = '/export/data/des50.b/data/BLISS/{dirname}/{expnum}/D00{expnum}_*_fullcat.fits'
+bliss_des60 = '/data/des60.b/data/BLISS/{dirname}/{expnum}/D00{expnum}_*_fullcat.fits'
+
+outtemp = '%(unitname)s_%(band)s.fits'
+
+DTYPES = [('FILENAME', 'S48'), ('PFW_ATTEMPT_ID', '>i8'), ('TAG', 'S13'), ('UNITNAME', 'S9'), ('REQNUM', '>i4'), ('ATTNUM', '>i2'), ('EXPNUM', '>i8'), ('CCDNUM', '>i2'), ('BAND', 'S1'), ('T_EFF', '>f4'), ('FWHM_WORLD', '>f4'), ('FLAGS', '>i2'), ('OBJECT_NUMBER', '>i4'), ('RA', '>f8'), ('DEC', '>f8'), ('FLUX_PSF', '>f4'), ('FLUXERR_PSF', '>f4'), ('FLUX_AUTO', '>f4'), ('FLUXERR_AUTO', '>f4'), ('A_IMAGE', '>f4'), ('B_IMAGE', '>f4'), ('THETA_IMAGE', '>f4'), ('CLASS_STAR', '>f4'), ('SPREAD_MODEL', '>f4'), ('SPREADERR_MODEL', '>f4'),('IMAFLAGS_ISO','>i2'),('MJD_OBS','>f8'),('EXPTIME','>f4')]
+
+def get_dirname(expnum):
+    return expnum//100 * 100
+
+def bliss_selection(data):
+    sel = data['FLUX_PSF'] > 0
+    sel &= data['FLUX_AUTO'] > 0
+    sel &= data['FLAGS'] < 4
+    sel &= (data['IMAFLAGS_ISO'] & 2047) == 0 
+    sel &= 1.0857362*(data['FLUXERR_PSF']/data['FLUX_PSF']) < 0.5
+    return sel
+    
+def create_output(data,exp):
+    out = np.recarray(len(data),dtype=DTYPES)
+    for n in out.dtype.names:
+        if n not in exp.dtype.names + data.dtype.names:
+            msg = "Column not found: %s"%n
+            raise Exception(msg)
+
+        if n in exp.dtype.names:
+            out[n] = exp[n]
+
+        if n in data.dtype.names:
+            out[n] = data[n]
+
+    return out
+
+def create_catalog(filename):
+    fits = fitsio.FITS(filename)
+
+    hdr = create_header(fits)
+    data = fits['LDAC_OBJECTS'].read()
+
+    cat = np.recarray(len(data),dtype=DTYPES)
+
+    for name,dtype in DTYPES:
+        if name not in data.dtype.names: continue
+        cat[name] = data[name]
+            
+    cat['FILENAME'] = os.path.basename(filename)
+    cat['PFW_ATTEMPT_ID'] = int(str(hdr['EXPNUM'])+'01')
+    cat['TAG'] = 'BLISS'
+    cat['UNITNAME'] = 'D%08d'%hdr['EXPNUM']
+    cat['REQNUM'] = 1
+    cat['ATTNUM'] = 1
+    cat['EXPNUM'] = hdr['EXPNUM']
+    cat['CCDNUM'] = hdr['CCDNUM']
+    cat['BAND'] = hdr['BAND']
+    cat['EXPTIME'] = hdr['EXPTIME']
+    cat['MJD_OBS'] = hdr['MJD-OBS']
+    cat['T_EFF'] = -1
+    cat['RA'] = data['ALPHAWIN_J2000']
+    cat['DEC'] = data['DELTAWIN_J2000']
+    cat['BAND'] = hdr['FILTER']
+    return cat
+
+def create_header(fits):
+    from astropy.io.fits import Header
+
+    if isinstance(fits,basestring):
+        fits = fitsio.FITS(fits)
+
+    hdrstr = '\n'.join(fits['LDAC_IMHEAD'].read()[0][0])
+    return Header.fromstring(hdrstr,sep='\n')
+
+def downskim(outfile,select,exp,force):
+    """ Like download, but for skimming catalog files.
+    """
+    if os.path.exists(outfile) and not force:
+        logging.warn('Found %s; skipping...'%outfile)
+        return
+
+    expnum = exp['EXPNUM']
+    dirname = get_dirname(expnum)
+    params = dict(expnum=expnum,dirname=dirname)
+    
+    filenames = glob.glob(bliss_des50.format(**params))
+    if not filenames:
+        filenames = glob.glob(bliss_des60.format(**params))
+    if not filenames:
+        msg = "File not found for: ",params
+        raise IOError(msg)
+
+    data = []
+    for f in filenames:
+        data += [create_catalog(f)]
+    data = np.hstack(data)
+
+    sel = select(data)
+    output = create_output(data[sel],exp)
+
+    logging.info("Writing %s..."%outfile)
+    fitsio.write(outfile,output)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('explist')
+    parser.add_argument('outfile')
+    parser.add_argument('-f','--force',action='store_true')
+    parser.add_argument('-e','--expnum',default=None,type=int)
+    parser.add_argument('-v','--verbose',action='store_true')
+    parser.add_argument('-t','--tag',default='BLISS',choices=TAGS)
+    args = parser.parse_args()
+
+    level = logging.DEBUG if args.verbose else logging.INFO
+    logging.getLogger().setLevel(level)
+
+    explist = pd.read_csv(args.explist).to_records()
+    exp = explist[explist['EXPNUM'] == args.expnum]
+
+    if len(exp) == 0:
+        raise ValueError("EXPNUM not found: %s"%args.expnum)
+    elif len(exp) > 1:
+        raise ValueError("Multiple values for EXPNUM found: %s"%args.expnum)
+    exp = exp[0]
+
+    if args.tag == 'BLISS':
+        select = bliss_selection
+    else:
+        raise Exception('Tag not found: %s'%args.tag)
+
+    downskim(args.outfile,select,exp,args.force)
---
+++ code/run_01.1_downskim.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+import os
+import yaml
+import subprocess
+import time
+import glob
+
+import numpy as np
+from utils import mkdir
+
+if __name__ == "__main__":
+    import argparse
+    description = "python script"
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('config')
+    parser.add_argument('-f','--force',action='store_true')
+    parser.add_argument('-s','--sleep',default=2,type=float)
+    parser.add_argument('-n','--njobs',default=None,type=int)
+    parser.add_argument('-q','--queue',default='condor')
+    parser.add_argument('-v','--verbose',action='store_true')
+    args = parser.parse_args()
+
+    config = yaml.load(open(args.config))
+    rawdir = config['rawdir']
+    explist = config['explist']
+    tags = config.get('tags')
+    
+    if os.path.exists(explist) and not args.force:
+        print "Found %s; skipping download..."%explist
+    else:
+        msg = "Missing exposure list: %s"%explist
+        raise Exception(msg)
+
+    exposures = np.recfromcsv(explist)
+    for band in config['bands']:
+        outdir = mkdir(os.path.join(rawdir,band))
+        logdir = mkdir(os.path.join(outdir,'log'))
+        for exp in exposures[exposures['band'] == band]:
+            if (tags is not None) and (exp['tag'] not in tags):
+                continue
+            unitname = 'D%08d'%(exp['expnum'])
+            outbase = '%s_%s_cat.fits'%(unitname,band)
+            outfile = os.path.join(outdir,outbase)
+            if os.path.exists(outfile) and not args.force:
+                print 'Found %s; skipping...'%outfile
+                continue
+
+            logfile = os.path.join(logdir,outbase.replace('.fits','.log'))
+            params = dict(expnum=exp['expnum'],tag=exp['tag'],
+                          force='-f' if args.force else '',
+                          verbose='-v' if args.verbose else '',
+                          outfile=outfile,explist=explist)
+                      
+            cmd = "downskim.py %(force)s %(verbose)s -t %(tag)s -e %(expnum)s %(explist)s %(outfile)s"%params
+            if args.queue == 'local':
+                submit = cmd
+            else:
+                submit = 'csub -o %s '%(logfile)
+                if args.njobs: submit += '-n %s '%args.njobs
+                submit += cmd
+            if args.verbose: print submit
+            subprocess.call(submit,shell=True)
+            time.sleep(args.sleep)
+
+            #break
+        #break
+        #time.sleep(300)
+        

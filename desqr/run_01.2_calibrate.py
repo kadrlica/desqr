59a695febd328608078d03d680b096a3c2f56ac7
Alex Drlica-Wagner <kadrlica@fnal.gov>
2020-04-17 14:51:41 -0500

    restructuring to chunk

--- desqr/run_01.2_calibrate.py
+++ desqr/run_01.2_calibrate.py
@@ -15,6 +15,10 @@ import download
 if __name__ == "__main__":
     import parser
     parser = parser.Parser(description=__doc__)
+    parser.add_argument('-k','--chunk',default=None,type=int,
+                        help='number of exposures to run in a chunk')
+    parser.add_argument('-b','--band',default=None,type=str,action='append',
+                        help='band to submit')
     args = parser.parse_args()
 
     config = yaml.safe_load(open(args.config))
@@ -23,33 +27,44 @@ if __name__ == "__main__":
     tags = config.get('tags')
     section = config.get('db','bliss')
 
+    chunk = 1 if args.chunk is None else args.chunk
+
     exposures = np.recfromcsv(explist)
-    for band in config['bands']:
+    bands = config['bands'] if args.band is None else args.band
+
+    for band in bands:
+        expband = exposures[exposures['band'] == band]
+        
         outdir = mkdir(os.path.join(zpsdir,band))
         logdir = mkdir(os.path.join(outdir,'log'))
-        for exp in exposures[exposures['band'] == band]:
-            print("WARNING: selecting on expnum!")
-            if exp['expnum'] < 600000: continue
-            if (tags is not None) and (exp['tag'] not in tags):
-                print("No exposures with tag '%s'"%tags)
-                continue
-            outbase = config['zpsbase']%exp
+
+        for exp in np.array_split(expband,len(expband)//chunk):
+            edict = dict(zip(exp[0].dtype.names,exp[0]))
+
+            outbase = config['zpsbase']
             outfile = os.path.join(outdir,outbase)
+
+            # If only one exposure, should skip if done
+            if len(exp)==1: outfile = outfile.format(**edict)
             if os.path.exists(outfile) and not args.force:
-                print 'Found %s; skipping...'%outfile
+                print('Found %s; skipping...'%outfile)
                 continue
 
+            # Create a logfile named after the first exposure
             logfile = os.path.join(logdir,os.path.splitext(outbase)[0]+'.log')
-            params = dict(expnum=exp['expnum'],tag=exp['tag'],
+            logfile = logfile.format(**edict)
+
+            expnums = ' '.join(str(e) for e in exp['expnum'])
+            params = dict(expnum=expnums,
                           force='-f' if args.force else '',
                           verbose='-v' if args.verbose else '',
                           outfile=outfile,explist=explist)
 
-            cmd = "calibrate.py %(force)s %(verbose)s -t %(tag)s -e %(expnum)s %(explist)s %(outfile)s"%params
-            submit = 'csub -q %s -o %s '%(args.queue, logfile)
-            if args.njobs: submit += '-n %s '%args.njobs
+            cmd = "calibrate.py %(force)s %(verbose)s %(explist)s %(outfile)s -e %(expnum)s "%params
+            submit = "csub -q %s -o %s "%(args.queue, logfile)
+            if args.njobs: submit += "-n %s "%args.njobs
             submit += cmd
-            if args.verbose: print submit
+            if args.verbose: print(submit)
             subprocess.call(submit,shell=True)
             time.sleep(args.sleep)
 

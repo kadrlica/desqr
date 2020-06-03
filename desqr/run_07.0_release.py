3891b8812ff1edb92c23553d0b9810e00f695d46
Alex Drlica-Wagner <kadrlica@fnal.gov>
2020-06-03 15:39:04 -0500

    release code

--- desqr/run_07.0_release.py
+++ desqr/run_07.0_release.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+"""
+Create release validation checks.
+"""
 import os
 import yaml
 import subprocess
@@ -7,29 +10,20 @@ import glob
 
 import numpy as np
 import healpy
-from ugali.utils.shell import mkdir
-from utils import found
-from const import BANDS
 
 SECTIONS = ['footprint','astrometry','depth']
 
 if __name__ == "__main__":
-    import argparse
-    description = "python script"
-    parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('config')
-    parser.add_argument('-f','--force',action='store_true')
-    parser.add_argument('-s','--sleep',default=2,type=float)
-    parser.add_argument('-q','--queue',default='local')
-    parser.add_argument('-r','--run',action='append',choices=SECTIONS)
-    parser.add_argument('-v','--verbose',action='store_true')
-    parser.add_argument('-n','--nside',default=None,type=int)
+    from parser import Parser
+    parser = Parser(description=__doc__)
     parser.add_argument('-b','--band',default=None,action='append')
+    parser.add_argument('--nside',default=None,type=int)
+    parser.add_argument('-r','--run',action='append',choices=SECTIONS)
     args = parser.parse_args()
-    
+
     force = '-f' if args.force else ''
 
-    config = yaml.load(open(args.config))
+    config = yaml.safe_load(open(args.config))
     sections = args.run if args.run else SECTIONS
     path = os.path.dirname(os.path.abspath(__file__))
 
@@ -37,7 +31,7 @@ if __name__ == "__main__":
         exe = os.path.join(path,'footprint.py')
         survey = config.get('survey','des')
         nside = 2048 if args.nside is None else args.nside
-        cmd = 'python %s %s -n %i --survey %s'%(exe,args.config,nside,survey)
+        cmd = 'python %s %s -p -n %i --survey %s'%(exe,args.config,nside,survey)
         if args.verbose: cmd += ' -v'
         if args.band: bands = args.band
         else:         bands = config['bands'] + [''.join(config['bands'])]
@@ -53,7 +47,7 @@ if __name__ == "__main__":
     if 'depth' in sections:
         nside = 1024 if args.nside is None else args.nside
         exe = os.path.join(path,'depth.py')
-        cmd = 'python %s -n %i'%(exe,nside)
+        cmd = 'python %s %s -n %i'%(exe,args.config,nside)
         if args.verbose: cmd += ' -v'
 
         if args.queue == 'local':
@@ -67,7 +61,12 @@ if __name__ == "__main__":
     if 'astrometry' in sections:
         exe = os.path.join(path,'astrometry.py')
         # r-band only
-        cmd = 'python %s %s --band r'%(exe,args.config)
+        if args.band: bands = args.band
+        else:         bands = ['r']
+        
+        cmd = 'python %s %s'%(exe,args.config)
+        cmd += ' '+' '.join(['-b '+b for b in bands])
+
         if args.verbose: cmd += ' -v'
 
         if args.queue == 'local':

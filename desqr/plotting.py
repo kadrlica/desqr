6275f80d3201635fa478723933c96c79efaae433
Alex Drlica-Wagner <kadrlica@fnal.gov>
2021-05-05 07:11:22 -0500

    skymap plotting

--- desqr/plotting.py
+++ desqr/plotting.py
@@ -11,9 +11,8 @@ import numpy as np
 import scipy.ndimage as nd
 import pylab as plt
 import matplotlib.colors as colors
-import healpy
+import healpy as hp
 
-from ugali.utils.healpix import ang2pix,pix2ang
 from ugali.utils.shell import mkdir
 
 from const import OBJECT_ID, UNIQUE_ID, BANDS, BADMAG, NSIDES
@@ -29,7 +28,7 @@ def draw_hist(hpxmap,**kwargs):
     if isinstance(hpxmap,np.ma.MaskedArray):
         pix = np.where(~hpxmap.mask)
     else:
-        pix = np.where((np.isfinite(hpxmap)) & (hpxmap !=healpy.UNSEEN))
+        pix = np.where((np.isfinite(hpxmap)) & (hpxmap !=hp.UNSEEN))
 
     data = hpxmap[pix]
     kwargs.setdefault('bins',np.linspace(data.min(),data.max(),100))
@@ -62,7 +61,7 @@ def draw_footprint(hpxmap,proj='car',**kwargs):
     Draw plot of footprint.
     """
     if not isinstance(hpxmap,np.ma.MaskedArray):
-        mask = ~np.isfinite(hpxmap) | (hpxmap==healpy.UNSEEN)
+        mask = ~np.isfinite(hpxmap) | (hpxmap==hp.UNSEEN)
         hpxmap = np.ma.MaskedArray(hpxmap,mask=mask)
     pix = np.where(~hpxmap.mask)
 
@@ -76,12 +75,12 @@ def draw_footprint(hpxmap,proj='car',**kwargs):
     extent = kwargs.pop('extent',[180,-180,-90,20])
     xmin,xmax,ymin,ymax = extent
 
-    nside = healpy.npix2nside(len(hpxmap))
+    nside = hp.npix2nside(len(hpxmap))
 
     steps = 500
     xx,yy = np.meshgrid(np.linspace(xmin,xmax,steps),
                         np.linspace(ymin,ymax,steps))
-    pp = ang2pix(nside,xx,yy)
+    pp = hp.ang2pix(nside,xx,yy,lonlat=True)
 
     ax = plt.gca()
     im = ax.pcolormesh(xx[::-1],yy,hpxmap[pp],**kwargs)
@@ -91,13 +90,12 @@ def draw_footprint(hpxmap,proj='car',**kwargs):
     ax.set_ylim(ymin,ymax)
 
     ax.grid(ls=':',color='black',lw=0.5)
-    return im
+    return im, xx, yy, hpxmap[pp]
 
-def draw_survey(hpxmap,**kwargs):
+def draw_skymap(hpxmap,**kwargs):
     from skymap.survey import SurveySkymap
     smap = SurveySkymap()
     return smap.draw_hpxmap(hpxmap,**kwargs)
-
 
 def draw_des(hpxmap,**kwargs):
     """ 
@@ -129,23 +127,33 @@ def draw_bliss(hpxmap,**kwargs):
     smap.draw_bliss()
     return smap.draw_hpxmap(hpxmap,**kwargs)
 
+def draw_delve(hpxmap,**kwargs):
+    """ 
+    Draw BLISS footprint:
+       360 > RA > 120, -60 < DEC < -10
+    """
+    from skymap.survey import DelveSkymap
+    smap = DelveSkymap()
+    smap.draw_milky_way()
+    return smap.draw_hpxmap(hpxmap,**kwargs)
+
 def draw_desgw(hpxmap,proj='car',**kwargs):
     kwargs.setdefault('extent',[180,30,-80,-50])
     return draw_footprint(hpxmap,proj,**kwargs)
 
-def draw_survey(survey,hpxmap,**kwargs):
+def draw_survey(hpxmap,survey=None,**kwargs):
     kwargs.setdefault('cmap','viridis')
     if survey == 'des': return draw_des(hpxmap,**kwargs)
     elif survey == 'maglites': return draw_maglites(hpxmap,**kwargs)
     elif survey == 'bliss': return draw_bliss(hpxmap,**kwargs)
     elif survey == 'delve': return draw_bliss(hpxmap,**kwargs)
-    else: return draw_survey(hpxmap,**kwargs)
+    else: return draw_skymap(hpxmap,**kwargs)
 
 def draw_pixel(hpxmap,**kwargs):
     if isinstance(hpxmap,np.ma.MaskedArray):
         pix = np.where(~hpxmap.mask)
     else:
-        pix = np.where((np.isfinite(hpxmap)) & (hpxmap !=healpy.UNSEEN))
+        pix = np.where((np.isfinite(hpxmap)) & (hpxmap !=hp.UNSEEN))
 
     vmin,vmax = np.percentile(hpxmap[pix],[0.5,99.5])
     kwargs.setdefault('vmin',vmin)
@@ -153,16 +161,16 @@ def draw_pixel(hpxmap,**kwargs):
     kwargs.setdefault('rasterized',True)
     kwargs.setdefault('cmap','jet')
 
-    nside = healpy.npix2nside(len(hpxmap))
-    pixrad = np.degrees(healpy.max_pixrad(nside))
-    ra,dec = pix2ang(nside,pix)
+    nside = hp.npix2nside(len(hpxmap))
+    pixrad = np.degrees(hp.max_pixrad(nside))
+    ra,dec = hp.pix2ang(nside,pix,lonlat=True)
 
     xmin,xmax = ra.min()-pixrad,ra.max()+pixrad
     ymin,ymax = dec.min()-pixrad,dec.max()+pixrad
 
     delta = 0.01
     xx,yy = np.meshgrid(np.arange(xmin,xmax,delta),np.arange(ymin,ymax,delta))
-    pp = ang2pix(nside,xx,yy)
+    pp = hp.ang2pix(nside,xx,yy,lonlat=True)
 
     ax = plt.gca()
     im = ax.pcolormesh(xx[::-1],yy,hpxmap[pp],**kwargs)
